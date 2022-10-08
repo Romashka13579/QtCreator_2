5cc63e8913b2decb2bea6fc36a5475ced0b029bc
Romashka <rroma13579@gmail.com>
2022-10-08 11:47:43 +0200

    fourth commit

---
+++ QtProject_4/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_lineEdit_textEdited(const QString &arg1)
+{
+    QString text = ui->lineEdit->text();
+    ui->statusbar->showMessage(text);
+}
+
